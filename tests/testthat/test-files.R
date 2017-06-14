415d6759c44573fb2791500416c6777396918ede
Michael Sumner <mdsumner@gmail.com>
2017-06-14 13:16:15 +1000

    err message

--- tests/testthat/test-files.R
+++ tests/testthat/test-files.R
@@ -2,5 +2,5 @@ context("files")
 
 fname <- paste(sample(unlist(strsplit("somecrazyfile", ""))), collapse = "")
 test_that("file not found is friendly", {
-  expect_error(tidync(fname), "cannot find file:")
+  expect_error(tidync(fname), "No such file or directory")
 })
