bf9d63fb1168f334a429f3e670d31465228124b9
Michael Sumner <mdsumner@gmail.com>
2017-06-19 16:48:14 +1000

    tes tilfes

--- tests/testthat/test-files.R
+++ tests/testthat/test-files.R
@@ -2,5 +2,10 @@ context("files")
 
 fname <- paste(sample(unlist(strsplit("somecrazyfile", ""))), collapse = "")
 test_that("file not found is friendly", {
-  expect_error(tidync(fname), "cannot find file:")
+  expect_error(tidync(fname), "No such file or directory")
+})
+
+test_that("files and sets of files are handled", {
+  fname <- raadtools::sstfiles()$fullname[1]
+  tidync(fname)
 })
