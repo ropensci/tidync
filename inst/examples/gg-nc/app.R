d04709647008f0b2305d8e8c0f31071e749aa111
Michael Sumner <mdsumner@gmail.com>
2017-06-28 00:13:51 +1000

    lapply input

--- inst/examples/gg-nc/app.R
+++ inst/examples/gg-nc/app.R
@@ -21,10 +21,11 @@ ui <- fluidPage(
         DT::dataTableOutput("nctable"),
         verbatimTextOutput("tidy_summary"),
         verbatimTextOutput("filter_summary"),
+        
         tabsetPanel( 
         tabPanel("plot", 
-                 textInput("filter_exps", "hyper_filter(...)", value = ""),
-                 textInput("aes_exps", "aes(...)", value = ""),
+                 uiOutput("filter_controls"),
+                 verbatimTextOutput("index_summary"),
                  plotOutput("ggplot")),
         tabPanel("help", verbatimTextOutput("help_text"))
       )
@@ -38,10 +39,56 @@ server <- function(input, output) {
        "1. Enter regex searches to hone in on files/paths.", 
        "2. See set of matching files in table below, explore them.",
        "3. Modify method to resolve final (single) file choice from available matches.", 
-       "4. Found a problem? Get in touch: https://github.com/hypertidy/tidync/issues", 
+       "4. Found a problem? Got ideas? Get in touch: https://github.com/hypertidy/tidync/issues", 
        "", "", 
-       "Powered by raadsync, raadtools, tidync, RNetCDF, ncdf4, shiny and the tidyverse.",
+       "A rancid production, powered by raadsync, raadtools, tidync, RNetCDF, ncdf4, shiny and the tidyverse.",
        "Michael Sumner, Australian Antarctic Division, Antarctic Climate and Ecosystems CRC, Hobart."))
+   })
+   output$filter_controls <- renderUI({
+     #cities <- getNearestCities(input$lat, input$long)
+     #checkboxGroupInput("cities", "Choose Cities", cities)
+    s_tab <- get_summary_table()
+    ndims <- nrow(s_tab)
+    ord <- c(t(matrix(seq_len(nrow(s_tab) * 2), 2)))
+  c(  lapply(seq_len(ndims), function(i) 
+       numericInput(sprintf("%sminval", s_tab$name[i]), sprintf("%s min",  s_tab$name[i]), 
+                                             value =  s_tab$min[i], min = s_tab$min[i], max = s_tab$max[i]))
+    ,
+    lapply(seq_along(nms), function(i) 
+      numericInput(sprintf("%smaxval",  s_tab$name[i]), sprintf("%s max",  s_tab$name[i]), 
+                   value = s_tab$max[i], min = s_tab$min[i], max = s_tab$max[i]))
+  )[ord]  
+#     numericInput("minvals", "minval", value = 0, min = -2, max = 4)
+   })
+   get_index <- reactive({
+    # data.frame(x = 1)
+      index <- get_filter() 
+      
+       summ <- get_summary_table()
+   #     nms <- summ$name
+   #     print(nms)
+   #    for (i in seq_along(nms)) {
+   #      vals <- index[[nms[i]]][[nms[i]]] 
+   #       index[[nms[i]]] <- index[[nms[i]]][ vals  >= summ$min[i] & vals <= summ$max[i], ]
+   #     }
+   #  hyper_index(index)
+   # summ
+   })
+   get_summary_table <- reactive({
+     index <- get_filter()
+     nms <- names(index)
+     minimums <- unlist(lapply(nms, function(x) min(index[[x]][[x]])))
+     maximums <- unlist(lapply(nms, function(x) max(index[[x]][[x]])))
+     input_min_names <- sprintf("%sminval", nms)
+     input_max_names <- sprintf("%smaxval", nms)
+     
+     ## STUCK here, not sure the input object is responding well 
+     #minimums <- pmin(minimums, unlist(lapply(input_min_names, function(a) input[[a]])))
+     #maximums <- pmax(maximums, unlist(lapply(input_max_names, function(a) input[[a]])))
+     tibble::tibble(name = nms, min = minimums, max = maximums, imx = input_max_names, imn = input_min_names)
+   })
+   output$index_summary <- renderPrint({
+     print(get_index())
    })
    output$tidy_summary <- renderPrint({
      print(get_tidync())
