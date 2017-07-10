c23394897f9c5dbec9f8fa42b21bae598c3782b4
Michael Sumner <mdsumner@gmail.com>
2017-07-10 13:19:43 +1000

    fix open handle problem https://github.com/hypertidy/tidync/issues/42

--- R/hyper_slice.R
+++ R/hyper_slice.R
@@ -1,39 +1,51 @@
 #' hyper slice
 #' 
+#' By default all variables in the active grid are returned, use `select_var` to limit. 
 #' @param x a hyperindex-able thing
 #' @param ... ignored
+#' @param select_var optional vector of variable names to select
 #' @param raw_datavals logical to control whether scaling in the NetCDF is applied or not
+#'
 #' @export
-hyper_slice <- function(x, ..., raw_datavals = FALSE) {
+hyper_slice <- function(x, select_var = NULL, ..., raw_datavals = FALSE) {
   UseMethod("hyper_slice")
 }
 #' @name hyper_slice
 #' @export
-hyper_slice.hyperindex <- function(x, ..., raw_datavals = FALSE) {
-  ## FIXME: the change to grid means we get x$grid[1] not variable, we have
-  ## to slice over all variables in this space
-  ## https://github.com/hypertidy/tidync/issues/33
-  varnames <- x$variable[[1]]
-  stats::setNames(lapply(varnames, function(vara)  
-    ncdf4::ncvar_get(ncdf4::nc_open(x$file[1]), vara, 
+hyper_slice.hyperindex <- function(x, select_var = NULL, ..., raw_datavals = FALSE) {
+  if (is.null(select_var))   {
+    varnames <- x$variable[[1]]
+  } else {
+    if (!any(select_var %in% x$variable[[1]])) {
+      print("available variables: ")
+      print(paste(x$variable[[1]], collapse = ", "))
+      stop(sprintf("some select_var variables not found %s", select_var))
+    }
+    varnames <- select_var
+  }
+  get_vara <- function(vara)  {
+    con <- ncdf4::nc_open(x$file[1])
+    on.exit(ncdf4::nc_close(con), add =   TRUE)
+    ncdf4::ncvar_get(con, vara, 
                      start = x$start, count = x$count, 
                      raw_datavals = raw_datavals)
-  ), varnames)
+}
+  stats::setNames(lapply(varnames, get_vara), varnames)
  }
 #' @name hyper_slice
 #' @export
-hyper_slice.hyperfilter <- function(x, ..., raw_datavals = FALSE) {
-  hyper_index(x, ...) %>% hyper_slice(raw_datavals = raw_datavals)
+hyper_slice.hyperfilter <- function(x, select_var = NULL, ..., raw_datavals = FALSE) {
+  hyper_index(x, ...) %>% hyper_slice(select_var = select_var, raw_datavals = raw_datavals)
 }
 #' @name hyper_slice
 #' @export
-hyper_slice.tidync <- function(x, ..., raw_datavals = FALSE) {
-  x %>% hyper_filter(...) %>% hyper_index() %>% hyper_slice(raw_datavals = raw_datavals)
+hyper_slice.tidync <- function(x, select_var = NULL, ..., raw_datavals = FALSE) {
+  x %>% hyper_filter(...) %>% hyper_index() %>% hyper_slice(select_var = select_var, raw_datavals = raw_datavals)
 }
 #' @name hyper_slice
 #' @export
-hyper_slice.character <- function(x, ..., raw_datavals = FALSE) {
-  tidync(x) %>% hyper_filter(...) %>%  hyper_index() %>% hyper_slice(raw_datavals = raw_datavals)
+hyper_slice.character <- function(x, select_var = NULL, ..., raw_datavals = FALSE) {
+  tidync(x) %>% hyper_filter(...) %>%  hyper_index() %>% hyper_slice(select_var = select_var, raw_datavals = raw_datavals)
 }
 
 
