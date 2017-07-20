3feda41eaed492bc7002f1a4a8ebb758e9b4733f
Michael Sumner <mdsumner@gmail.com>
2017-07-20 21:49:04 +1000

    oceandata opendap R32 L3m SST example

--- tests/testthat/test-thredds.R
+++ tests/testthat/test-thredds.R
@@ -9,5 +9,12 @@ test_that("thredds works", {
   unc %>% activate("D2")
   
   unc %>% hyper_filter(longitude = index < 100, latitude = latitude > 60, time = index == 1)
+
+  ## example thanks to Ben Tupper
+  uri <- 'https://oceandata.sci.gsfc.nasa.gov:443/opendap/MODISA/L3SMI/2016/001/A20160012016032.L3m_R32_SST_sst_9km.nc'
+  
+  (nc <- tidync(uri))
   
 })
+
+
