cf1fef4d6f28017b6e93ed16869a9a0c04ea7771
Michael Sumner <mdsumner@gmail.com>
2017-11-30 10:01:57 +1100

    unicorn

--- R/filter-groupby.R
+++ R/filter-groupby.R
@@ -2,12 +2,12 @@
 #' @importFrom rlang .data
 #' @importFrom raster setValues
 fast_cellnumbers <- function(r, p) {
-  #p <- sf::st_as_sf(p) 
-  #p$ID <- seq_len(nrow(p))
-  #r <- raster::setValues(r, 0)
-  #rr <- fasterize::fasterize(p, r, field = "ID")
-  #tabularaster::as_tibble(rr) %>% dplyr::filter(!is.na(cellvalue)) %>% 
-  #  dplyr::rename(object_ = .data$cellvalue, cell_ = .data$cellindex)
+  p <- sf::st_as_sf(p)
+  p$ID <- seq_len(nrow(p))
+  r <- raster::setValues(r, 0)
+  rr <- fasterize::fasterize(p, r, field = "ID")
+  tabularaster::as_tibble(rr) %>% dplyr::filter(!is.na(cellvalue)) %>%
+   dplyr::rename(object_ = .data$cellvalue, cell_ = .data$cellindex)
 }
 
 #' Hyper group by
@@ -20,23 +20,26 @@ fast_cellnumbers <- function(r, p) {
 #' @importFrom raster extent xmin xmax ymin ymax
 #' @noRd
 hyper_group_by <- function(x, object, ns = 1:2) {
-  hf <- hyper_filter(x)
-  if (!is.null(attr(hf, "nominal_space"))) stop("object already has a nominated space!")
-  ext <- raster::extent(object)
-  xynames <- names(hf)[ns]
-  xs <- hf[[1]][[xynames[1]]]
-  ys <- hf[[2]][[xynames[2]]]
-  hf[[1]] <- hf[[1]] %>% dplyr::filter(between(xs, xmin(ext), xmax(ext))) 
-  hf[[2]]  <- hf[[2]] %>% dplyr::filter( between(ys, ymin(ext), ymax(ext)))
-  xs <- hf[[1]][[xynames[1]]]
-  ys <- hf[[2]][[xynames[2]]]
-  nominal_space <- tibble::as_tibble(expand.grid(x = xs, y = ys))
+  nominal_space <- active_axis_transforms(x)
+  ext <- try(raster::extent(object), silent = TRUE)
+  if (inherits(ext, "try-error") & "sf" %in% class(object)) {
+    ext <- raster::extent(attributes(object[[attr(object, "sf_column")]])[["bbox"]][c("xmin", "xmax", "ymin", "ymax")])
+  }
+  xynames <- names(nominal_space)[ns]
+  xs <- nominal_space[[1]][[xynames[1]]]
+  ys <- nominal_space[[2]][[xynames[2]]]
+  nominal_space[[1]] <- nominal_space[[1]] %>% dplyr::filter(between(xs, raster::xmin(ext), raster::xmax(ext))) 
+  nominal_space[[2]]  <- nominal_space[[2]] %>% dplyr::filter(between(ys, raster::ymin(ext), raster::ymax(ext)))
+  xs <- nominal_space[[1]][[xynames[1]]]
+  ys <- nominal_space[[2]][[xynames[2]]]
+  nominal_space <- tibble::as_tibble(expand.grid(x = xs, y = rev(ys)))
   cn <- fast_cellnumbers(raster::rasterFromXYZ(nominal_space), object)
-  ok <- rep(FALSE, nrow(nominal_space))
-  ok[cn$cell_] <- TRUE
-  nominal_space$ok <- ok
-  attr(hf, "nominal_space") <- nominal_space
-  
-  hf 
+  #ok <- rep(FALSE, nrow(nominal_space))
+  #ok[cn$cell_] <- TRUE
+  object <- rep(NA_integer_, nrow(nominal_space))
+  object[cn$cell_] <- cn$object_
+  nominal_space$object <- object
+  #attr(hf, "nominal_space") <- nominal_space
+  nominal_space 
 }
 
