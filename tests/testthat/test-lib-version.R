0e169c745b10b65439d4b4c4af7795d77ae3be70
Michael Sumner <mdsumner@gmail.com>
2019-03-05 09:51:06 +1100

    test in case version no capable

---
+++ tests/testthat/test-lib-version.R
@@ -0,0 +1,14 @@
+context("test-lib-version")
+
+test_that("robust to version capabilites", {
+  ufile <- system.file("extdata", "unidata", "test_hgroups.nc", 
+    package = "tidync", mustWork = TRUE)
+  ## some versions of NetCDF 4.. don't support this file
+  ## (4.1.3 https://github.com/hypertidy/tidync/issues/82)
+  group_nc <- try(tidync(ufile), silent = TRUE)
+  if (inherits(group_nc, "test-error")) {
+    expect_true(grepl("NetCDF: Invalid dimension ID or name", unclass(group_nc)[1]))
+  } else {
+    expect_s3_class(group_nc, "tidync")
+  }
+  })
