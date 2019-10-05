42e23f78a7db18463359e9c4f8974010dc1f8f59
Michael Sumner <mdsumner@gmail.com>
2019-10-05 22:49:28 +0000

    try https

--- tests/testthat/test-thredds.R
+++ tests/testthat/test-thredds.R
@@ -4,7 +4,7 @@ test_that("thredds works", {
   skip_on_travis()
   skip_on_cran()
   library(tidync)
-  u <- "http://coastwatch.pfeg.noaa.gov/erddap/griddap/erdQSwind3day"
+  u <- "https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdQSwind3day"
   unc <- tidync(u)
   expect_named(unc, c("source", "axis", "grid", 
                       "dimension", "variable", "transforms"))
