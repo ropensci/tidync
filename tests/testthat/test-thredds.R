7f858f2b0056a2f9cf406a13b162052b4d4593c2
Michael Sumner <mdsumner@gmail.com>
2019-10-23 00:07:10 +1100

    thredds

--- tests/testthat/test-thredds.R
+++ tests/testthat/test-thredds.R
@@ -3,13 +3,12 @@ context("thredds")
 test_that("thredds works", {
   skip_on_travis()
   skip_on_cran()
+  skip_on_appveyor()
   library(tidync)
   u <- "https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdQSwind3day"
   unc <- tidync(u)
   expect_named(unc, c("source", "axis", "grid", 
                       "dimension", "variable", "transforms"))
-  
-  
 })
 
 
