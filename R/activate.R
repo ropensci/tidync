310acbec5ce5d7685b524c3429770079c85a1d17
Adrian Odenweller <adrian.odenweller@pik-potsdam.de>
2022-04-01 19:55:18 +0200

    Enable grid activation by using a variable name as a string

--- R/activate.R
+++ R/activate.R
@@ -61,7 +61,10 @@ activate.tidync <- function(.data, what, ..., select_var = NULL) {
   } else {
    vargrids <- tidyr::unnest(.data$grid) 
   }
-  what_name <- deparse(substitute(what))
+  # Try to set what_name to what (in case it's a string)
+  what_name <- try(what, silent = T)
+  # If it fails, use deparse(substitute(what)) to turn into string
+  if (class(what_name) == "try-error") what_name <- deparse(substitute(what))
   #if (what_name %in% var_names(.data)) what <- what_name
   if (what_name %in% vargrids$variable) {
     ## use the variable to find the grid
